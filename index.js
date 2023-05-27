037ca19c372b72e390868099ec387abbc070a269
Alberico Fagundes <alberico@fagundes.live>
2023-05-27 01:36:08 -0300

    criado rotina enviar sticker

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { create } from "venom-bot";
+import sharp from "sharp";
+import fs from "fs";
 
 // Criação do cliente do WhatsApp
 const createOption = {
@@ -15,11 +17,43 @@ create(createOption)
     console.log("erro\n", erro);
   });
 
+async function commands(client, message) {
+  const iaCommands = {
+    sticker: "/sticker",
+  };
+
+  if (message.type == "image" && message.caption === "/sticker") {
+    const imgBuffer = await client.decryptFile(message);
+    console.log("imgBuffer", imgBuffer);
+
+    fs.writeFileSync("output.webp", imgBuffer);
+
+    sharp(imgBuffer)
+      .toFormat("webp") // Converter para formato webp
+      .toBuffer()
+      .resize(200)
+      .then((webpBuffer) => {
+        fs.writeFileSync("output.webp", webpBuffer);
+
+        client
+          .sendImageAsSticker(message.chatId, "./output.webp")
+          .then((result) => {
+            console.log("Result: ", result); //return object success
+          })
+          .catch((erro) => {
+            console.error("Error when sending: ", erro); //return object error
+          });
+      })
+      .catch((error) => {
+        // Tratar erros, se necessário
+        console.error(error);
+      });
+  }
+}
+
 // Função que inicia o cliente do WhatsApp e trata as mensagens recebidas
 async function start(client) {
-  console.log("Start Client\n");
   client.onAnyMessage((message) => {
-    console.log("Start Client\n");
     commands(client, message);
   });
 }
