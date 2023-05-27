5d9928c7422e8a7a001188b379390fcfc41191d5
Alberico Fagundes <alberico@fagundes.live>
2023-05-26 22:44:35 -0300

    criado cliente venom

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 import { create } from "venom-bot";
+
+// Criação do cliente do WhatsApp
+const createOption = {
+  session: "my-session", // Define a opção session como "my-session"
+  useChrome: true,
+};
+
+create(createOption)
+  .then((client) => {
+    start(client); // Inicia o cliente de WhatsApp
+    console.log("Whats-Jarvis connected\n");
+  })
+  .catch((erro) => {
+    console.log("erro\n", erro);
+  });
+
+// Função que inicia o cliente do WhatsApp e trata as mensagens recebidas
+async function start(client) {
+  console.log("Start Client\n");
+  client.onAnyMessage((message) => {
+    console.log("Start Client\n");
+    commands(client, message);
+  });
+}
